a91860874bb57f946696e5190c207128f4c53d78
Riteshkumar2204 <rk8958496098@gmail.com>
2026-01-14 10:14:18 +0530

    update address

--- FRONTEND/src/components/Footer.jsx
+++ FRONTEND/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="banner">
           <div className="left">RITESH</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Lucknow</p>
+            <p>Z5 Gulshan-e-Maymar, Lucknow</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
